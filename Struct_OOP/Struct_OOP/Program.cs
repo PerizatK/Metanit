3ab4d88932806ec1b1d2c81d1a8797ba8e55582e
PerizatK <karina.perizat@gmail.com>
2020-07-28 18:15:39 +0600

    IdealWeightByBrokeIndex

---
+++ Struct_OOP/Struct_OOP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace IDealWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user;
+
+            do
+            {
+                Console.WriteLine("Please enter your age");
+                user.age = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter your height");
+                user.height = Int32.Parse(Console.ReadLine());
+
+                user.DisplayInfo();
+
+                Console.WriteLine("Try one more time? \n Yes: Y, No: N");
+                
+                if (Console.ReadLine() == "N")
+                { 
+                    break;
+                }
+                    }
+            while (1 == 1);
+
+
+            Console.ReadKey();
+        }
+
+        struct User
+        {
+            public int age;
+            public int height;
+
+            public void DisplayInfo()
+            {
+                int weight;
+                if (age < 40)
+                {
+                    weight = height - 110;
+                }
+                else
+                {
+                    weight = height - 100;
+                }
+                Console.WriteLine($"Your ideal weight equals to {weight}");
+            }
+
+        }
+    }
+}
